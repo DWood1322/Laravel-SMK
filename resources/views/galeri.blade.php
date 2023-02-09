d502f7acbe43082258036b88ccaaf63d950ca2b8
DWood1322 <echo.arlandanaufal1322@gmail.com>
2023-02-09 08:44:43 +0700

    First commit

---
+++ resources/views/galeri.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">       
+    <title>Data Siswa</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div class="container">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/siswa">Data Siswa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/guru">Data Guru</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/galeri">Gallery</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container text-center mt-4">
+        <img class="rounded-circle img-thumbnail m-2" src="/img/Heejin.jpg" alt="">
+        <img class="rounded-circle img-thumbnail m-2" src="/img/Eunha.jpg" alt=""><br>
+        <img class="rounded-circle img-thumbnail m-2" src="/img/Karina.jpg" alt="">
+        <img class="rounded-circle img-thumbnail m-2" src="/img/Kim.jpg" alt="">
+        
+
+    </div>
+    
+
+    
+
+</body>
+</html>
